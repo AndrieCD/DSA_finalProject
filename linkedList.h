e37f9a5d759df2237942cb60df730edaf3ce224e
AndrieCD <andriecd02@gmail.com>
2024-07-11 18:50:23 +0800

    working version

--- linkedList.h
+++ linkedList.h
@@ -8,7 +8,7 @@ template <typename T>
 struct Node
 {
     T data;
-    struct Node *next;
+    Node<T> *next;
 };
 
 template <typename T>
@@ -21,6 +21,25 @@ public:
     LinkedList()
     {
         head = NULL;
+    }
+
+    void updateFile()
+    {
+        Node<T> *current = head;
+        ofstream outFile("studentList.txt"); // output to file
+
+        current = head;
+
+        while (current)
+        { // do this code if the current is not NULL or empty
+            outFile << current->data.studentName << endl
+                    << current->data.age << endl
+                    << current->data.studentNumber << endl;
+
+            current = current->next;
+        }
+
+        outFile.close(); // close file
     }
 
     void append(T &newData)
@@ -44,43 +63,146 @@ public:
         }
     }
 
-    /*
-     Searches for an element in the list
-    bool search(int val)
+    void deleteRecord(const string &toSearch)
     {
-        // TODO
-        Node *nodePtr;
+        Node<T> *current = head;
+        Node<T> *prev = NULL;
 
-        if (!head)
-        {
-            return false;
+        if (!current)
+        { // if there is nothing on the list, show this message
+            cout << "NO RECORDS AVAILABLE..." << endl
+                 << endl;
+            system("pause");
+            return;
         }
-        else
-        {
-            nodePtr = head;
 
-            while (nodePtr)
+        while (current)
+        { // if current is not null do this code
+            if (current->data.studentName == toSearch)
+            { // checks if student idNumber is the same as the input
+                if (current != head)
+                { // if the chosen is not the head
+                    prev->next = current->next;
+                    delete current; // deletes the node
+                    cout << "Record Deleted!\n";
+                    return;
+                }
+                else
+                {                         // if the current is the head
+                    head = current->next; // give head to the next node
+                    delete current;       // delete the current node
+                    cout << "Record Deleted!\n";
+                    return;
+                }
+            }
+            prev = current;
+            current = current->next;
+        }
+
+        cout << toSearch << " was not found!\n";
+        return;
+    }
+
+    int listSize()
+    {
+        int size = 0;
+        Node<T> *nodePtr = head;
+
+        while (nodePtr)
+        {
+            size++;
+            nodePtr = nodePtr->next;
+        }
+        return size;
+    }
+
+    void bubbleSortAscending()
+    {
+        int n = listSize();
+        for (int i = 0; i < n - 1; ++i)
+        {
+            Node<T> *nodePtr = head;
+            for (int j = 0; j < n - i - 1; ++j)
             {
-                if (nodePtr->data == val)
+                if (nodePtr->data.studentName > nodePtr->next->data.studentName)
                 {
-                    return true;
+                    string temp = nodePtr->data.studentName;
+                    nodePtr->data.studentName = nodePtr->next->data.studentName;
+                    nodePtr->next->data.studentName = temp;
                 }
                 nodePtr = nodePtr->next;
             }
         }
-        // default return value
-        return false;
     }
-    */
 
-    void display() const
+    void bubbleSortDescending()
     {
-        Node<T> *temp = head;
-        while (temp)
+        int n = listSize();
+        for (int i = 0; i < n - 1; ++i)
         {
-            std::cout << "Item: " << temp->data.itemName << ", Price: " << temp->data.price << ", Stock: " << temp->data.numInStock << std::endl;
-            temp = temp->next;
+            Node<T> *nodePtr = head;
+            for (int j = 0; j < n - i - 1; ++j)
+            {
+                if (nodePtr->data.studentName < nodePtr->next->data.studentName)
+                {
+                    string temp = nodePtr->data.studentName;
+                    nodePtr->data.studentName = nodePtr->next->data.studentName;
+                    nodePtr->next->data.studentName = temp;
+                }
+                nodePtr = nodePtr->next;
+            }
         }
+    }
+
+    void search(const string &toSearch)
+    {
+        Node<T> *nodePtr = head;
+        if (!nodePtr)
+        {
+            cout << "The list is empty!";
+            return;
+        }
+        while (nodePtr)
+        {
+            if (nodePtr->data.studentName == toSearch)
+            {
+                cout << "Student Found!" << endl
+                     << endl
+                     << "   Student: " << nodePtr->data.studentName << endl
+                     << "       Age: " << nodePtr->data.age << endl
+                     << " ID Number: " << nodePtr->data.studentNumber << endl
+                     << endl;
+                return;
+            }
+            nodePtr = nodePtr->next;
+        }
+        cout << toSearch << " was not found!";
+        return;
+    }
+
+    void traversalDisplay(Node<T> *nodePtr, int studNum)
+    {
+        if (!nodePtr)
+        {
+            return;
+        }
+
+        cout << "Student " << studNum << ": " << nodePtr->data.studentName << endl
+             << "       Age: " << nodePtr->data.age << endl
+             << " ID Number: " << nodePtr->data.studentNumber << endl
+             << endl;
+        traversalDisplay(nodePtr->next, ++studNum);
+    }
+
+    void display()
+    {
+        if (!head)
+        {
+            cout << "The list is empty!";
+            return;
+        }
+        int studNum = 1;
+        traversalDisplay(head, studNum);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 #include "linkedList.h"
 
 using namespace std;
 
-struct menuItem
+struct studentInfo
 {
-    string itemName;
-    float price;
-    int numInStock;
+    string studentName;
+    int age;
+    unsigned int studentNumber;
+
+    void getData()
+    {
+        cout << "Enter Student's Name: ";
+        getline(cin, studentName);
+
+        cout << "Enter Student's Age: ";
+        cin >> age;
+
+        cout << "Enter Student's Student Number: ";
+        cin >> studentNumber;
+    }
 };
 
-void loadMenuItems(LinkedList<menuItem> &menuList)
+void pauseClear()
+{ // code is for pausing and clearing the screen
+    system("pause");
+    system("cls");
+}
+
+void loadstudentInfos(LinkedList<studentInfo> &studentList)
 {
-    ifstream inFile("menu.txt");
+    ifstream inFile("studentList.txt");
 
     if (!inFile.is_open())
-    {           // if the file cannot be opened...
-        return; // early return, completely stop the function once error is encountered
+    {
+        return;
     }
 
-    std::string itemName;
-    float price;
-    int numInStock;
+    std::string studentName;
+    int age;
+    unsigned int studentNumber;
 
-    while (getline(inFile, itemName) && inFile >> price >> numInStock)
+    while (getline(inFile, studentName) && inFile >> age >> studentNumber)
     {
-        menuItem item = {itemName, price, numInStock};
-        menuList.append(item);
+        studentInfo student = {studentName, age, studentNumber};
+        studentList.append(student);
 
         inFile.ignore(100, '\n');
     }
@@ -36,11 +55,165 @@ void loadMenuItems(LinkedList<menuItem> &menuList)
     inFile.close();
 }
 
+void addNewRecord(LinkedList<studentInfo> &studentList, studentInfo student)
+{
+    ofstream outFile("studentList.txt", ios::app);
+
+    studentList.append(student);
+
+    outFile
+        << student.studentName << endl
+        << student.age << endl
+        << student.studentNumber << endl;
+
+    outFile.close();
+    cout << "Student Added!\n";
+}
+
+void mainMenu(int &menuChoice)
+{
+
+    // textual displays about the program and the options that can be made
+    do
+    {
+        cout << "---------------------------------------------------------------------------" << endl
+             << "                           STUDENT INFORMATION SYSTEM" << endl
+             << "---------------------------------------------------------------------------" << endl
+             << endl
+
+             << "What do you want to do?" << endl
+             << "1. Add New Record" << endl
+             << "2. Search Record" << endl
+             << "3. Display All Records" << endl
+             << "4. Sort Records" << endl
+             << "5. Delete Record" << endl
+             << "6. Exit" << endl
+             << endl;
+
+        cout << "Your Input Here: "; // input prompt
+
+        // INPUT VALIDATION
+        do
+        {
+            while (!(cin >> menuChoice))
+            { // Repeat until a valid integer input is received.
+                cout << "Enter a valid numeric input: ";
+                cin.clear();
+                cin.ignore(123, '\n');
+            }
+
+            if (menuChoice > 6 || menuChoice < 1)
+            {                                            // If the input does not fall inside the menu's available options...
+                cout << "Enter a valid numeric input: "; // prompt the user to enter a valid input.
+            }
+
+        } while (menuChoice > 6 || menuChoice < 1); // Repeat the loop until a valid input is acquired.
+
+        if (menuChoice == 6)
+            break;
+
+    } while (menuChoice < 1 || menuChoice > 6); // run the program as long as the user's input is out of bounds or invalid
+
+    system("cls"); // clears the screen after successful input
+}
+
 int main()
 {
-    LinkedList<menuItem> menuList;
-    loadMenuItems(menuList);
-    menuList.display();
+    LinkedList<studentInfo> studentList;
+    studentInfo studentData;
+
+    int menuChoice;
+    string toSearch;
+
+    loadstudentInfos(studentList);
+
+    do
+    {
+
+        mainMenu(menuChoice); // run the mainMenu function to display it on the screen
+
+        switch (menuChoice)
+        { // depending on the user's input in mainMenu(), menuChoice will go through the switch case
+        case 1:
+            cout << "---------------------------------------------" << endl
+                 << "                  Add Record                 " << endl
+                 << "---------------------------------------------" << endl
+                 << endl;
+            cin.clear();
+            cin.ignore(123, '\n');
+            studentData.getData();                  // gets the student data from the user. A member function of the class.
+            addNewRecord(studentList, studentData); // processes the data into the addNewRecord function for linked list creation
+            pauseClear();
+            break;
+
+        case 2:
+            cout << "---------------------------------------------" << endl
+                 << "                Search Record                " << endl
+                 << "---------------------------------------------" << endl
+                 << endl;
+
+            cout << "Search by name: ";
+
+            cin.clear();
+            cin.ignore(123, '\n');
+            getline(cin, toSearch);
+
+            studentList.search(toSearch);
+            pauseClear();
+            break;
+
+        case 3:
+            cout << "=====================================================" << endl
+                 << "                   DISPLAY RECORDS                   " << endl
+                 << "=====================================================" << endl
+                 << endl;
+            studentList.display(); // display all records
+            pauseClear();
+            break;
+
+        case 4:
+            cout << "=====================================================" << endl
+                 << "                    SORT RECORDS                     " << endl
+                 << "=====================================================" << endl
+                 << endl;
+            cout << "Type A to sort students from A-Z\n"
+                 << "Type Z to sort students from Z-A\n";
+            cout << "Your Input Here: "; // input prompt
+            char input;
+            cin >> input;
+            if (input == 'A')
+            {
+                studentList.bubbleSortAscending();
+            }
+            else if (input == 'Z')
+            {
+                studentList.bubbleSortDescending();
+            }
+            cout << "Sorting is Successful!\n";
+            pauseClear();
+            break;
+
+        case 5:
+            cout << "=====================================================" << endl
+                 << "                Delete Record" << endl
+                 << "=====================================================" << endl
+                 << endl;
+            cin.clear();
+            cin.ignore(123, '\n');
+            cout << "Enter the name of the student: ";
+            getline(cin, toSearch);
+            studentList.deleteRecord(toSearch); // deletes a specific record based on user input (ID number)
+            pauseClear();
+            break;
+        case 6:
+            studentList.updateFile();
+            cout << "MEMBERS: " << endl
+                 << "Casuga, Nolan Simon" << endl
+                 << "Detera, Andrie" << endl
+                 << "Regero, Angelito Jose" << endl;
+            break; // breaks the do-while loop for program exit
+        }
+    } while (menuChoice != 6);
 
     return 0;
 }
